d8d15dbd9efc58271b5d920044ec19ab8fe21ec7
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2016-05-02 18:49:56 +0200

    No need of alist-get for polymode polymode:#96

--- init.el
+++ init.el
@@ -483,16 +483,6 @@ layers configuration. You are free to put any user code."
   ;;; remove automatic matching of backtics
   (sp-local-pair "`" nil :actions nil)
   ;;; 
-  (if (< emacs-major-version 25)
-	(defun alist-get (key alist &optional default remove)
-  "Get the value associated to KEY in ALIST.
-DEFAULT is the value to return if KEY is not found in ALIST.
-REMOVE, if non-nil, means that when setting this element, we should
-remove the entry if the new value is `eql' to DEFAULT."
-  (ignore remove) ;;Silence byte-compiler.
-  (let ((x (assq key alist)))
-    (if x (cdr x) default)))
-)
 
 )
 
