66159346ec270a313b5db6329f4e881815c37bc0
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2016-05-02 18:47:12 +0200

    Add julia-mode implementation as in #96

--- polymode/packages.el
+++ polymode/packages.el
@@ -12,7 +12,8 @@
 
 (defun polymode/init-polymode ()
   (use-package polymode
-    :mode (("\\.[rR]md"   . Rmd-mode))
+    :mode (("\\.[rR]md" . Rmd-mode))
+    :mode (("\\.[jJ]md" . poly-markdown-mode))
     :init
     (progn
       (defun Rmd-mode ()
@@ -23,6 +24,8 @@
         (R-mode)
         (poly-markdown+r-mode))
       ))
+    (with-eval-after-load 'polymode
+	(define-polymode poly-markdown+julia-mode pm-poly/markdown :lighter " PM-jmd"))
   )
 
 ;;; packages.el ends here
